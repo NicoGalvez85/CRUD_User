465f61326a6bd26c36c6ed8c5132d7c62225a2e1
Nicolas <nicolasgalvez1985@gmail.com>
2023-02-20 19:49:26 -0300

    Se protegen datos locales

--- .env.php
+++ .env.php
@@ -2,7 +2,7 @@
 function credenciales() {
     return [
         'usuario'=>'root' ,
-        'clave'=>'npEGa2014',
+        'clave'=>'', //la contraseña de la base de datos
         'servidor'=>'localhost',
         'base_de_datos'=>'ejerciciomsql'
     ];
